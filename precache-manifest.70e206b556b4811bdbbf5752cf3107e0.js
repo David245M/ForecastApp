87b33baa21e95e5bc1c4831b6d2815a500e09f23
Mike <mike@users.noreply.github.com>
2020-06-17 04:34:07 +0300

    Updates

---
+++ precache-manifest.70e206b556b4811bdbbf5752cf3107e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347c804bd043d6ffb90c2177dcc7b6d1",
+    "url": "/ForecastApp/index.html"
+  },
+  {
+    "revision": "fee275a58a700f39b5b0",
+    "url": "/ForecastApp/static/css/main.c4fc3a3b.chunk.css"
+  },
+  {
+    "revision": "dd355fd7e49b1edecce7",
+    "url": "/ForecastApp/static/js/2.e5aaea84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ForecastApp/static/js/2.e5aaea84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee275a58a700f39b5b0",
+    "url": "/ForecastApp/static/js/main.3f178c06.chunk.js"
+  },
+  {
+    "revision": "02c8f5bb3e9434aed4d0",
+    "url": "/ForecastApp/static/js/runtime-main.3ea2a21e.js"
+  }
+]);
