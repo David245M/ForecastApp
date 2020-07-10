9bd91ffab20103a2a9e726c9564fdd5da7e20efb
Mike <mike@users.noreply.github.com>
2020-07-10 03:04:10 +0300

    Updates

---
+++ precache-manifest.b19858949231c48f995e0dfe2499c0c6.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9388264b541367417ca7028350b53735",
+    "url": "/ForecastApp/index.html"
+  },
+  {
+    "revision": "499f0099122c673aa991",
+    "url": "/ForecastApp/static/css/main.0291e79b.chunk.css"
+  },
+  {
+    "revision": "fe34926d6e55973cf854",
+    "url": "/ForecastApp/static/js/2.f0bc6b0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ForecastApp/static/js/2.f0bc6b0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "499f0099122c673aa991",
+    "url": "/ForecastApp/static/js/main.49417b0b.chunk.js"
+  },
+  {
+    "revision": "aa46ec2f428c61ae252a",
+    "url": "/ForecastApp/static/js/runtime-main.748b0f0e.js"
+  },
+  {
+    "revision": "6f8e40cc7800a647e0de77ad87da0462",
+    "url": "/ForecastApp/static/media/cloudy-weather.6f8e40cc.gif"
+  },
+  {
+    "revision": "3e03ef73c02bf65d539c9928cb0dc75c",
+    "url": "/ForecastApp/static/media/cloudy-weather_200_transparent.3e03ef73.gif"
+  },
+  {
+    "revision": "9fcfbc69c6bf357db96c45f1890e4728",
+    "url": "/ForecastApp/static/media/day-night-weather_200_transparent.9fcfbc69.gif"
+  },
+  {
+    "revision": "aa2e8f97a6c17d1a01d51d30ee8f8338",
+    "url": "/ForecastApp/static/media/fog-weather_200_transparent.aa2e8f97.gif"
+  },
+  {
+    "revision": "bb308ea9b4f82545f4e35ecee2c5a7d2",
+    "url": "/ForecastApp/static/media/haze-weather_200_transparent.bb308ea9.gif"
+  },
+  {
+    "revision": "43ee7eab4b12666cca42dc850ac13989",
+    "url": "/ForecastApp/static/media/light-rain-cloud-weather_200_transparent.43ee7eab.gif"
+  },
+  {
+    "revision": "43ac538b053013a96c0bd6b8407c33bd",
+    "url": "/ForecastApp/static/media/light-rain-weather_200_transparent.43ac538b.gif"
+  },
+  {
+    "revision": "d56f5ba7f2d431dbab671493ffcb3c3a",
+    "url": "/ForecastApp/static/media/light-snowy-weather_200_transparent.d56f5ba7.gif"
+  },
+  {
+    "revision": "bf180d85ce40f01d948ebc48389c6945",
+    "url": "/ForecastApp/static/media/lightning-bolt-weather_200.bf180d85.gif"
+  },
+  {
+    "revision": "b6e277ee654da4522d0051363dbb67aa",
+    "url": "/ForecastApp/static/media/lightning-weather_200_transparent.b6e277ee.gif"
+  },
+  {
+    "revision": "61d5872e477194729a90ea787b0c19c8",
+    "url": "/ForecastApp/static/media/night-weather.61d5872e.gif"
+  },
+  {
+    "revision": "f43790beab2d1178986f6c5e8fb89e0e",
+    "url": "/ForecastApp/static/media/night-weather_200_transparent.f43790be.gif"
+  },
+  {
+    "revision": "3830f09db4f9b3f188492144647b42e5",
+    "url": "/ForecastApp/static/media/rain-cloud-weather-2_200_transparent.3830f09d.gif"
+  },
+  {
+    "revision": "95c3066aaa733c252a665d47fed8c586",
+    "url": "/ForecastApp/static/media/rain-cloud-weather_200_transparent.95c3066a.gif"
+  },
+  {
+    "revision": "070dbf455379091afac5715b6d44dfba",
+    "url": "/ForecastApp/static/media/rain.070dbf45.gif"
+  },
+  {
+    "revision": "26b428f36f6bc208ecb65061ac3adf4e",
+    "url": "/ForecastApp/static/media/rainy-weather_200_transparent.26b428f3.gif"
+  },
+  {
+    "revision": "c6e84204b53f46e7a60b483ec4829b55",
+    "url": "/ForecastApp/static/media/shower-rain.c6e84204.gif"
+  },
+  {
+    "revision": "96ea9765611dca65ddb8fcd866fdeab1",
+    "url": "/ForecastApp/static/media/snow-storm-weather_200_transparent.96ea9765.gif"
+  },
+  {
+    "revision": "64cba3318e91754fc6ca57c3121acf1c",
+    "url": "/ForecastApp/static/media/snow.64cba331.gif"
+  },
+  {
+    "revision": "893867f3d239198de179e3e8a4e2c71c",
+    "url": "/ForecastApp/static/media/stormy-weather_200_transparent.893867f3.gif"
+  },
+  {
+    "revision": "ec26d49b923a4f01137f44c54a2584ec",
+    "url": "/ForecastApp/static/media/sun-weather.ec26d49b.gif"
+  },
+  {
+    "revision": "fcc0dfcb90c2d6351efa4605309e8892",
+    "url": "/ForecastApp/static/media/sun-weather_200_transparent.fcc0dfcb.gif"
+  },
+  {
+    "revision": "33fbbfb47dc995f39cf9f2c9c238a609",
+    "url": "/ForecastApp/static/media/sunrise-weather_200_transparent.33fbbfb4.gif"
+  },
+  {
+    "revision": "a7717a9c674898ec8600d1d87f1d16e3",
+    "url": "/ForecastApp/static/media/sunset-weather_200_transparent.a7717a9c.gif"
+  },
+  {
+    "revision": "616d655747f330d57c5bd2f6a6b5bc0c",
+    "url": "/ForecastApp/static/media/thunderstorm.616d6557.gif"
+  },
+  {
+    "revision": "545ddc392b34b37c5f7ca4378c4af4e2",
+    "url": "/ForecastApp/static/media/torrential-rain-weather_200_transparent.545ddc39.gif"
+  },
+  {
+    "revision": "5f3f33072c2f7573fc947c28fbf80cd7",
+    "url": "/ForecastApp/static/media/windy-cloud-weather_200_transparent.5f3f3307.gif"
+  },
+  {
+    "revision": "336e8537d362a3595c98faf4fd511e6b",
+    "url": "/ForecastApp/static/media/windy-weather_200_transparent.336e8537.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ForecastApp/precache-manifest.ec17bfbfcf49972cc1db5389b10ea5eb.js"
+  "/ForecastApp/precache-manifest.b19858949231c48f995e0dfe2499c0c6.js"
 );
 
 self.addEventListener('message', (event) => {
